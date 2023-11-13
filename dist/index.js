b8c2f9db4f2657e40f5319ca46149a61f7302b0e
AntonZinchenko32 <zinchenko309@gmail.com>
2023-11-13 18:08:31 +0200

    ?

--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,19 @@ if (button && input) {
         concatenation(input.value, 'hello!');
     });
 }
+class Component {
+    constructor(props) {
+        this.props = props;
+    }
+}
+class Page extends Component {
+    pageInfo() {
+        console.log(this.props.title);
+    }
+}
+const myTitle = {
+    title: 'test'
+};
+const pageInstance = new Page(myTitle);
+pageInstance.pageInfo();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,31 @@ if (button && input) {
     concatenation(input.value, 'hello!');
   });
 }
+
+/*
+  Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
+*/
+
+interface Props {
+  title:string
+}
+
+class Component<T> {
+  constructor (public props:T) {
+    
+  }
+}
+
+class Page extends Component<Props> {
+  pageInfo ():void {
+    console.log(this.props.title);
+  }
+}
+
+const myTitle = {
+  title:'test'
+}
+
+const pageInstance = new Page(myTitle);
+
+pageInstance.pageInfo();
